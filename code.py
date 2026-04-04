354a0bb888a7262cbc779cab35c86d05a24fd1fc
niccit <ntynen+github@gmail.com>
2026-04-04 11:44:34 -1000

    Addressed a few bugs after moving to light sleep from exit & deep sleep. Cleaned up mute/unmute. Added off (black) for pixel fill. Moved setting LED keypad colors to a function which accepts a color (for turning LEDs "off")

--- code.py
+++ code.py
@@ -59,13 +59,10 @@ hdmi_source = adafruit_led_animation.color.CYAN
 optical_source = adafruit_led_animation.color.JADE
 prog_rock = adafruit_led_animation.color.PURPLE
 aloha_joe = adafruit_led_animation.color.RED
+off = adafruit_led_animation.color.BLACK
 
 # 4 key NeoKey
 keypad = NeoKey1x4(i2c, addr=0x30)
-keypad.pixels[0] = optical_source
-keypad.pixels[1] = hdmi_source
-keypad.pixels[2] = prog_rock
-keypad.pixels[3] = aloha_joe
 
 # ---- Bluesound Node ---- #
 baseURL = os.getenv("bluesound_baseUrl")
@@ -149,6 +146,24 @@ my_mqtt.on_message = on_message
 battery_feed = os.getenv("battery_feed")
 
 # ---- Helpers ---- #
+# Set the keypad leds
+keypad_pixels = False   # True = on, False = off
+def set_leds(color=None):
+    global keypad_pixels
+    if color is None:
+        keypad.pixels[0] = optical_source
+        keypad.pixels[1] = hdmi_source
+        keypad.pixels[2] = prog_rock
+        keypad.pixels[3] = aloha_joe
+        keypad_pixels = True
+    else:
+        keypad.pixels[0] = color
+        keypad.pixels[1] = color
+        keypad.pixels[2] = color
+        keypad.pixels[3] = color
+        onboardLED.fill(color)
+        keypad_pixels = False
+
 # Return the last volume level
 def get_volume():
     volumeResponse = requests.get(baseURL + volumeQuery)
@@ -193,13 +208,20 @@ def send_request(url):
 my_mqtt.connect()
 increment = int(volume_increment)  # mirror the Android app, increase volume by 2 with each turn
 is_playing = False
-sleeping = time.monotonic()
-logger.debug(f"sleeping starting and is {sleeping}")
+last_volume = None
 wait_to_sleep = 300
 logger.info("Bluesound companion starting up!")
 onboardLED = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=1)
 
 while True:
+    # Active LEDs get set to black when we light sleep
+    # Check to see if they need to be reset
+    if not keypad_pixels:
+        logger.debug("setting led colors and resetting sleeping time")
+        sleeping = time.monotonic()
+        logger.debug(f"sleeping starting and is {sleeping}")
+        set_leds()
+
     position = -encoder.position    # turn clockwise to increase, counter-clockwise to decrease
 
     # Setting the volume on the bluesound node does not directly relate to the position of the
@@ -208,16 +230,16 @@ while True:
         last_position = position
         logger.debug(f"position is {position} and last_position is {last_position}")
 
+    volumeLevel = get_volume()  # get current volume level
+    device_volume = int(volumeLevel)  # convert to an int for maths
     if last_position != position:
         logger.debug(f"last position {last_position} does not equal position {position}")
-        volumeLevel = get_volume()  # get current volume level
-        volume = int(volumeLevel)  # convert to an int for maths
-        logger.debug(f"volume is {volume}")
         if position < last_position:    # lower the volume
             logger.debug(f"{position} < {last_position}")
+            logger.debug(f"volume is {device_volume}")
             last_position = position
             logger.debug(f"last position is {last_position}")
-            new_volume = volume - int(volume_increment)
+            new_volume = device_volume - int(volume_increment)
             logger.debug(f"new_volume is {new_volume}")
             if new_volume >= 0:
                 volume_url = f"{baseURL}{volumeChange}{new_volume}"
@@ -226,7 +248,8 @@ while True:
             logger.debug(f"{position} > {last_position}")
             last_position = position
             logger.debug(f"last position is {last_position}")
-            new_volume = volume + int(volume_increment)
+            logger.debug(f"volume is {device_volume}")
+            new_volume = device_volume + int(volume_increment)
             logger.debug(f"new_volume is {new_volume}")
             if new_volume <= 100:
                 logger.debug(f"new_volume is less than or equal to 100")
@@ -239,19 +262,24 @@ while True:
     # Store the volume prior to change
     # If the volume is zero, restore volume to last saved level
     if not button.value:
-        if volume != 0:
-            last_volume = volume
+        if device_volume != 0:
+            logger.debug(f"will mute volume, device volume is {device_volume}")
+            last_volume = device_volume
             mute_url = f"{baseURL}{volumeChange}0"
             logger.debug(f"url to send is mute_url: {mute_url}")
             send_request(mute_url)
-            volume = 0
         else:
+            logger.debug(f"will unmute volume, device volume is {device_volume}")
+            if last_volume is None:
+                logger.debug(f"last_volume is unset at {last_volume}, setting to 30")
+                last_volume = 30
             unmute_url = f"{baseURL}{volumeChange}{last_volume}"
             logger.debug(f"url to send is unmute_url: {unmute_url}")
             send_request(unmute_url)
-            volume = last_volume
         sleeping = time.monotonic()
         logger.debug(f"not button block sleeping is now {sleeping} seconds")
+        logger.debug(f"device volume is {get_volume()}")
+
 
     # Keypad
     # The third key is intentionally not programmed
@@ -268,7 +296,7 @@ while True:
         sleeping = time.monotonic()
         logger.debug(f"hdmi sleeping is now {sleeping} seconds")
     if keypad[2]:       # Play TuneIn favorite Aloha Joe Radio
-        logger.debug("tuning in to Prog Rock Radio")
+        logger.debug("tuning in to Prog Rock NL Radio")
         if not is_playing:
             keypad_url = baseURL + prog_rock_play
             send_request(keypad_url)
@@ -277,6 +305,8 @@ while True:
             keypad_url = baseURL + prog_rock_pause
             send_request(keypad_url)
             is_playing = False
+        sleeping = time.monotonic()
+        logger.debug(f"PRNL sleeping is now {sleeping} seconds")
     if keypad[3]:       # Play TuneIn favorite Aloha Joe Radio
         logger.debug("tuning in to Aloha Joe Radio")
         if not is_playing:
@@ -313,8 +343,13 @@ while True:
         logger.debug(f"battery warning {batteryVoltage:.2f} Volts, and battery warn is {batteryWarn}")
         batteryCheck = time.monotonic()
 
+    # Light sleep after a preset time
+    # Turn off all non-essential LEDs
     if time.monotonic() > sleeping + wait_to_sleep:
         hibernate_alarm = alarm.pin.PinAlarm(pin=nightNight_pin, value=False, edge=False, pull=True)
+        set_leds(off)
+        logger.debug("going into light sleep")
+        sleeping = time.monotonic()
         alarm.light_sleep_until_alarms(hibernate_alarm)
 
     time.sleep(0.25)
